76d850483b54671c715e72670ca981f6148028ca
Lukas C <mail@lukasc.de>
2021-12-01 15:09:43 +0100

    Initialization

---
+++ AdventOfCode/AdventOfCode/Services/FileReader.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AdventOfCode
+{
+    internal class FileReader
+    {
+        internal List<string[]> ReadFromCSV(string path)
+        {
+            List<string[]> contence = new List<string[]>();
+            if (File.Exists(path))
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    while (sr.Peek() >= 0)
+                    {
+                        string line = sr.ReadLine();
+                        string[] sa = line.Split(',');
+                        foreach (string s in sa)
+                        {
+                            s.Trim();
+                        }
+                        contence.Add(sa);
+                    }
+                }
+            }
+            return contence;
+        }
+
+        internal List<decimal> ReadDecFromListFile(string path)
+        {
+            List<decimal> contence = new List<decimal>();
+            if (File.Exists(path))
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    while (sr.Peek() >= 0)
+                    {
+                        string line = sr.ReadLine();
+                        decimal v = Decimal.Parse(line.Trim());
+                        contence.Add(v);
+                    }
+                }
+            }
+            return contence;
+        }
+
+        internal List<string> ReadLinesFromFile(string path)
+        {
+            List<string> contence = new List<string>();
+            if (File.Exists(path))
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    while (sr.Peek() >= 0)
+                    {
+                        contence.Add(sr.ReadLine());
+                    }
+                }
+            }
+            return contence;
+        }
+    }
+}
